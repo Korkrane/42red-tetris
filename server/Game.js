46e76708c02e72b9dd72ffaf58eb6fcd3a7274a8
Korkrane <baudoin.haas@gmail.com>
2022-11-04 15:26:42 +0100

    FIX: games works in front

--- server/Game.js
+++ server/Game.js
@@ -18,6 +18,11 @@ class Game{
     {
         return Array.from(Array(20), () => new Array(12).fill([0, 'clear']),);
     }
+
+    updateKey(key)
+    {
+        this.keyCode = key;
+    }
   }
 
 module.exports = Game;
